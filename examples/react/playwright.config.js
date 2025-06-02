fce9c917c2f36c1ae89cca40a8244f1667cf8f4d
RYN <frndakaryn@gmail.com>
2025-06-02 22:56:43 +0700

    Configure Playwright to output JUnit XML report

--- examples/react/playwright.config.js
+++ examples/react/playwright.config.js
@@ -23,11 +23,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'], // Ini akan menampilkan ringkasan di konsol pipeline
+    ['html'], // Ini akan tetap menghasilkan laporan HTML Playwright (yang sudah kamu publish sebagai artifact)
+    ['junit', { outputFile: 'test-results.xml' }] // Ini yang akan menghasilkan laporan JUnit XML
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    // baseURL: 'https://react.todomvc.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
